74fc2f1c52d815cf616fd7e01820ca8c18b70d5d
sasamoahkb <158211703+sasamoahkb@users.noreply.github.com>
2025-07-31 15:28:48 +0100

    Add files via upload

---
+++ demo/src/main/java/com/sasamoahkb/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.sasamoahkb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	// Additional methods or configurations can be added here if needed
+	@GetMapping
+	public String helloworld() {
+		return "Hello World Spring Boot!";
+	}
+
+
+
+}
